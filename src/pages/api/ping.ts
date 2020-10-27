209fbd771fd7f6a2821f4c58f6830a710d36845f
japser36 <jasper@looman.ca>
2020-10-26 20:18:53 -0700

    Added some sample code and documentation

---
+++ src/pages/api/ping.ts
@@ -0,0 +1,8 @@
+import pg from 'pg';
+const dbClient = pg.createClient();
+
+export default (req, res) => {
+	const q = db.query('SELECT * FROM blah WHERE ...')
+
+	res.status(200).json({ text: 'Pong!', data: q })
+}
