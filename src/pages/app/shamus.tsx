3cccb25c7008f81dc188365f66f66d55dd9acc43
Shamus Boulianne <shamus.boulianne@gmail.com>
2020-11-02 19:41:08 -0800

    Setup environment
    Setup environment and added a 'shamus' page which I will use for basic testing.

---
+++ src/pages/app/shamus.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+	return (
+		<>
+            <Head>
+				<title>Shamus Dev Page</title>
+			</Head>
+			<h1>Shamus' Dev Page</h1>
+			
+		</>
+	)
+}
