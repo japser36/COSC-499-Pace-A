644792d9fa4eb72d63538c5713f7bd7b66b78df3
Shamus Boulianne <shamus.boulianne@gmail.com>
2020-11-08 19:42:13 -0800

    commented out
    commented out ping.ts because it prevents workflow from passing.

--- src/pages/api/ping.ts
+++ src/pages/api/ping.ts
@@ -1,8 +1,10 @@
-import pg from 'pg'
-const dbClient = pg.createClient()
+// import pg from 'pg'
+// const dbClient = pg.createClient()
 
-export default (req, res) => {
-    const q = db.query('SELECT * FROM blah WHERE ...')
+// export default (req, res) => {
+//     const q = db.query('SELECT * FROM blah WHERE ...')
 
-    res.status(200).json({ text: 'Pong!', data: q })
-}
+//     res.status(200).json({ text: 'Pong!', data: q })
+// }
+
+// commented out because lack of pg module was preventing build during workflow.
