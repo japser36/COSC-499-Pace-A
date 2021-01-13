243b7be88eea2f4c76b438eb86cea9cb557d8e6c
Shamus Boulianne <shamus.boulianne@gmail.com>
2021-01-12 21:35:05 -0800

    Structured Database
    Created an ER diagram as a database structure proposal. Implemented this structure as sql in db_init.sql

--- src/sql/db_init.sql
+++ src/sql/db_init.sql
@@ -6,18 +6,32 @@ CREATE TABLE users (
 	email VARCHAR(127),
 	passHash VARCHAR(127),
 	userType VARCHAR(7),
-    verified BOOLEAN DEFAULT FALSE,
 	PRIMARY KEY(fb_uid)
 );
 
 CREATE TABLE mentee (
-
+	fb_uid VARCHAR(127),
+	mentor_id VARCHAR(127),
+	skills VARCHAR(255)
+	PRIMARY KEY(fb_uid)
 );
 
 CREATE TABLE mentor (
-
+	fb_uid VARCHAR(127),
+	adm_id VARCHAR(127),
+	skills VARCHAR(255)
+	PRIMARY KEY(fb_uid)
 );
 
-CREATE TABLE admin (
+CREATE TABLE org_admin (
+	fb_uid VARCHAR(127),
+	org_id VARCHAR(127),
+	PRIMARY KEY(fb_uid)
+);
 
+CREATE TABLE organization (
+	org_id VARCHAR(127),
+	org_name VARCHAR(127),
+	adm_id VARCHAR(127),
+	PRIMARY KEY(org_id)
 );
