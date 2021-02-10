d5758e57006e69ff153ed1f0ad5f0712d30df319
Shamus Boulianne <shamus.boulianne@gmail.com>
2021-02-10 13:24:07 -0800

    Changed skills api to use hardvoded skills rather than the Emsi api according to client feedback

--- src/pages/api/skills.ts
+++ src/pages/api/skills.ts
@@ -1,51 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import fetch from 'node-fetch'
-import qs from 'qs'
+import pool from '../../lib/db'
 
-export default async function skills(req: NextApiRequest, res: NextApiResponse) {
+export default async function getSkills(req: NextApiRequest, res: NextApiResponse) {
   // we will be responding with JSON in this file, declare this.
   res.setHeader('Content-Type', 'application/json')
 
-  const token = await getToken()
-  const query = qs.stringify({ q: req.query.filter, fields: 'name', limit: 1000 })
-  const url = 'https://emsiservices.com/skills/versions/latest/skills?' + query
-  const options = {
-    method: 'GET',
-    headers: { authorization: 'Bearer ' + token },
-  }
-  await fetch(url, options)
-    .then((response) => response.json())
-    .then(async (response) => {
-      await safeSend({ res, data: JSON.stringify(response.data) })
+  const sql = `SELECT * FROM skill`
+  const values = []
+
+  await pool
+    .query(sql, values)
+    .then(async (result) => {
+      const rows = result ? result.rows : null
+      await safeSend({ res, data: JSON.stringify({ success: true, rows }) })
     })
     .catch(async (error) => {
       await safeSend({ res, status: 400, data: JSON.stringify({ error: error.toString() }) })
     })
-}
-
-const getToken = async () => {
-  let token = null
-  const url = 'https://auth.emsicloud.com/connect/token'
-  const params = new URLSearchParams({
-    client_id: 'uocv0jx28sw2r99f',
-    client_secret: 'XeVmKper',
-    grant_type: 'client_credentials',
-    scope: 'emsi_open',
-  })
-  const options = {
-    method: 'POST',
-    headers: { 'content-type': 'application/x-www-form-urlencoded' },
-    body: params,
-  }
-  await fetch(url, options)
-    .then((response) => response.json())
-    .then((response) => {
-      token = response.access_token
-    })
-    .catch((error) => {
-      console.log(error.response.data.error)
-    })
-  return token
 }
 
 const safeSend = async ({
@@ -57,7 +28,7 @@ const safeSend = async ({
   status?: number
   data: string
 }) => {
-  //console.log(`Sending Response [${status}]:`, data)
+  console.log(`Sending Response [${status}]:`, data)
   if (res.headersSent) {
     console.warn('Stopped a response since the response was already sent!')
   } else {
