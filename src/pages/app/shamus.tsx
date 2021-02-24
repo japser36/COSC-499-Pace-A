a4740ab068ebcc95d527aeb656f82d84a0a843cd
Shamus Boulianne <shamus.boulianne@gmail.com>
2021-02-24 13:12:07 -0800

    test commit

--- src/pages/app/shamus.tsx
+++ src/pages/app/shamus.tsx
@@ -13,6 +13,6 @@ export default function Shamus({ data }) {
     </>
   )
 }
-
+//test
 //the following command is the only fix i could find for a recurring issue i have been having. I think this is an issue on my end only but idk for sure.
 //yarn add @types/jest @types/node @types/react @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-prettier eslint-plugin-react husky lint-staged prettier typescript jest ts-jest node-mocks-http cross-env
