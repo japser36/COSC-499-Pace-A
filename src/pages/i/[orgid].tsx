9b232d3fdb7bac44cdec93d44346e8764758e1b7
Maya <mayatomoum@gmail.com>
2021-03-02 18:10:20 -0800

    prettier formatting

--- src/pages/i/[orgid].tsx
+++ src/pages/i/[orgid].tsx
@@ -2,15 +2,9 @@ import { useRouter } from 'next/router'
 
 import UserSignUp from '../../components/UserSignUp'
 
-export default function Login() {
+export default function Login(): JSX.Element {
   const router = useRouter()
   const { orgid } = router.query
 
-
-  return (
-    <UserSignUp
-  userType='Mentee'
-  org_id={orgid}
-/>
-  )
+  return <UserSignUp userType="Mentee" org_id={orgid} />
 }
