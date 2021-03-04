878499ec3279d161153101336100ec503cfbd45b
Maya <mayatomoum@gmail.com>
2021-03-04 12:12:15 -0800

    draft
    couple more changes

--- src/pages/i/[orgid].tsx
+++ src/pages/i/[orgid].tsx
@@ -1,10 +1,17 @@
 import { useRouter } from 'next/router'
-
+import { useState } from 'react'
 import UserSignUp from '../../components/UserSignUp'
 
 export default function Login(): JSX.Element {
   const router = useRouter()
   const { orgid } = router.query
+  const [orgData, setOrgData] = useState(null)
+
+  if (orgData == null) {
+    //TODO: fetch organization data and set when it arrives
+
+    return <p> Loading... </p>
+  }
 
   return <UserSignUp userType="Mentee" org_id={orgid} />
 }
