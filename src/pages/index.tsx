3f665668601a7a9ea57df603f06c703bfb7204aa
Maya <mayatomoum@gmail.com>
2021-03-10 10:50:06 -0800

    Fixed container location and sizes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,21 @@
 import Link from 'next/link'
 import { useUser } from '../lib/auth/useUser'
 import Layout from '../components/layout'
-//import { useState } from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Button from '@material-ui/core/Button'
 
 const Home = () => {
   const { user, logout } = useUser()
-  //const [userType, setUserType] = useState('default')
+  const [userType, setUserType] = useState('default')
   const router = useRouter()
   return (
     <Layout home>
-      <p>{user ? 'You are signed in.' : 'You can sign in or sign up below.'}</p>
+      <p>{user ? 'You are signed in.' : 'You can sign in or sign up below.'}</p> <br></br>
       <>
         {user ? (
           <>
+            <br></br>
             <Link href="/app/loggedin">
               <a>View User Information</a>
             </Link>
@@ -22,11 +23,13 @@ const Home = () => {
             <Link href="/">
               <a onClick={() => logout()}>Sign Out</a>
             </Link>
+            <br></br>
           </>
         ) : (
           <div>
+            <br></br>
             <Button
-              variant="text"
+              variant="contained"
               onClick={() => {
                 router.push('/app/login')
               }}
@@ -35,13 +38,15 @@ const Home = () => {
             </Button>
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <Button
-              variant="text"
+              variant="contained"
               onClick={() => {
                 router.push('/app/register')
               }}
             >
               Sign Up
             </Button>
+            <br></br>
+            <br></br>
           </div>
         )}
       </>
