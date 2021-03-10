a104f1c8e046fc8c8a22b6e81de642ab90870185
Maya <mayatomoum@gmail.com>
2021-03-10 00:04:59 -0800

    Fixing Problems

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 import { useUser } from '../lib/auth/useUser'
 import Layout from '../components/layout'
-import { useState } from 'react'
+//import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Button from '@material-ui/core/Button'
 
 const Home = () => {
   const { user, logout } = useUser()
-  const [userType, setUserType] = useState('default')
+  //const [userType, setUserType] = useState('default')
   const router = useRouter()
   return (
     <Layout home>
